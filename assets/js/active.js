719dc198885ef8884437d747c425da82d7cad11a
Anantshah133 <anantshah130305@gmail.com>
2025-01-13 00:39:32 +0530

    changed header and created new files

--- assets/js/active.js
+++ assets/js/active.js
@@ -13,6 +13,12 @@ function setActiveClass(){
         "denmark.html": "nav-denmark",
         "ireland.html": "nav-ireland",
         "dubai.html": "nav-dubai",
+        "ielts-test.html" : "nav-ielts",
+        "pte-test.html" : "nav-pte",
+        "duolingo-test.html" : "nav-duolingo",
+        "toefl-test.html" : "nav-toefl",
+        "gmat-test.html" : "nav-gmat",
+        "gre-test.html" : "nav-gre",
     };
 
     if (navLinks[currentPage]) {
